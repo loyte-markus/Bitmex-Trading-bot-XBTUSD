20d4a51fe1d901d9db7abf2398df2b7b782e39d0
loyte-markus <32316026+loyte-markus@users.noreply.github.com>
2020-11-16 17:33:16 +0100

    Added more logging and did a small fix.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,7 +9,7 @@ namespace ConsoleApp1 {
   public class Program {
     private static string bitmexKey = System.IO.File.ReadAllText(@"C:\bitmexKey.txt");
     private static string bitmexSecret = System.IO.File.ReadAllText(@"C:\bitmexSecret.txt");
-    private const double BTC_per_trade = 0.02;
+    private const double BTC_per_trade = 0.025;
     private double stopLoss = 0;
     private double takeProfit = 0;
 
@@ -31,24 +31,28 @@ namespace ConsoleApp1 {
         
         if (order == null) {
           if(now.Minute == 00) {
+            //TODO: Legg til logikk på at vi ikke vil gå inn i samme trend flere ganger
             Log(now.ToString()+":");
-            var HH = now.Subtract(new TimeSpan(2, 0, 0)).ToString("HH");
+            var HH = now.Subtract(new TimeSpan(3, 0, 0)).ToString("HH");
+            var old4 = JsonConvert.DeserializeObject<List<TradeBucketed>>(bitmex.Get1hCandle(HH).ResponseData).FirstOrDefault();
+
+            HH = now.Subtract(new TimeSpan(2, 0, 0)).ToString("HH");
             var old3 = JsonConvert.DeserializeObject<List<TradeBucketed>>(bitmex.Get1hCandle(HH).ResponseData).FirstOrDefault();
 
             HH = now.Subtract(new TimeSpan(1, 0, 0)).ToString("HH");
             var old2 = JsonConvert.DeserializeObject<List<TradeBucketed>>(bitmex.Get1hCandle(HH).ResponseData).FirstOrDefault();
 
-            HH = now.Subtract(new TimeSpan(0, 0, 0)).ToString("HH");
-            var old1 = JsonConvert.DeserializeObject<List<TradeBucketed>>(bitmex.Get1hCandle(HH).ResponseData).FirstOrDefault();
+            var HHH = now.Hour.ToString();
+            var old1 = JsonConvert.DeserializeObject<List<TradeBucketed>>(bitmex.Get1hCandle(HHH).ResponseData).FirstOrDefault();
 
-            if(old3.Close > old2.Close  && old2.Close > old1.Close) {
+            if(old4.Close > old3.Close && old3.Close > old2.Close  && old2.Close > old1.Close) {
               Log("Placing short order...");
               var priceResponse = bitmex.GetPrice();
               errorCheck(priceResponse, args);
               var priceList = JsonConvert.DeserializeObject<List<Trade>>(priceResponse.ResponseData);
               double usdval = priceList.FirstOrDefault().Price * BTC_per_trade;
               bitmex.ShortOrder((int)usdval);
-            } else if(old1.Close> old2.Close && old2.Close > old3.Close) {
+            } else if(old1.Close > old2.Close && old2.Close > old3.Close && old3.Close > old4.Close) {
               Log("Placing long order...");
               var priceResponse = bitmex.GetPrice();
               errorCheck(priceResponse, args);
@@ -68,7 +72,7 @@ namespace ConsoleApp1 {
             if(takeProfit == 0) takeProfit = (long)order.AvgCostPrice * 0.99;
             if(priceList.FirstOrDefault().Price > stopLoss) {
               bitmex.closePosition();
-              Log("Order closed.");
+              Log(now + ": Order closed.");
               Log("Entry: " + order.AvgCostPrice + "Sold at ~"+ stopLoss);
               takeProfit = 0;
               stopLoss = 0;
@@ -76,7 +80,7 @@ namespace ConsoleApp1 {
             if(priceList.FirstOrDefault().Price < takeProfit) {
               stopLoss = takeProfit * 1.002;
               takeProfit = takeProfit * 0.995;
-              Log("Stop Loss & Take Profit ++");
+              Log(now + ": Stop Loss & Take Profit ++");
             }
           } else {
             //Long
@@ -86,12 +90,14 @@ namespace ConsoleApp1 {
               bitmex.closePosition();
               takeProfit = 0;
               stopLoss = 0;
-              Log("Order closed.");
+              Log(now + ": Order closed.");
+              Log("Entry: " + order.AvgCostPrice + "Sold at ~" + stopLoss);
+
             }
             if(priceList.FirstOrDefault().Price > takeProfit) {
               stopLoss = takeProfit * 0.998;
               takeProfit = takeProfit * 1.005;
-              Log("Stop Loss & Take Profit ++");
+              Log(now + ": Stop Loss & Take Profit ++");
             }
           }
         }
@@ -102,12 +108,11 @@ namespace ConsoleApp1 {
     private void errorCheck(APIResponse a, string[] args) {
       if(a.ToString().Contains("error")) {
         Thread.Sleep(5000);
+        Log("Re-running Run()...");
         Run(args);
       }
     }
-    private void setSLTP() {
 
-    }
     private void Log(string msg) {
       Console.WriteLine(msg);
     }
